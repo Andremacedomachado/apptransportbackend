e52bcacec405ed57c2e5be464583100c70d7610b
Andremacedomachado <andremacedomachado@gmail.com>
2022-09-05 14:31:07 -0300

    feat: Adicionado interface de aclopamento de repositorio de User.

---
+++ src/application/repositories/IUserRepository.ts
@@ -0,0 +1,11 @@
+import { User } from '../../domain/entities/users/User';
+
+export type UserId = {
+    id: string
+}
+
+export interface IUserRepository {
+    save(user: User): Promise<UserId | null>
+    findById(id: string): Promise<User | null>
+    findByEmail(email: string): Promise<User | null>
+}
