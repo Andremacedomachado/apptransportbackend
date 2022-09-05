c2633d2562516a92d195dce96542c5446c81d3e6
Andremacedomachado <andremacedomachado@gmail.com>
2022-09-05 14:40:49 -0300

    feat: Adicionado implementação de repositorio com prisma sobre user e entidade User.

---
+++ src/application/repositories/implementation/PrismaUserRepository.ts
@@ -0,0 +1,63 @@
+import { hash } from 'bcryptjs';
+import { IUserRepository, UserId } from '../IUserRepository';
+import { User } from '../../../domain/entities/users/User';
+import { prisma } from '../../../database';
+
+
+export class PrismaUserRepository implements IUserRepository {
+
+    async save(user: User): Promise<UserId | null> {
+        const { name, email, password } = user.props;
+        const hashPassword = await hash(password, 8);
+
+        const userCreated = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashPassword
+            }
+        });
+
+        return { id: userCreated.id };
+    }
+
+    async findById(id: string): Promise<User | null> {
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            }
+        });
+        if (!user) {
+            return null;
+        }
+        const userInMemory = User.create({
+            name: user.name,
+            email: user.email,
+            createdAt: user.createdat,
+            updatedAt: user.updatedAt,
+            password: user.password
+        }, user.id);
+        return userInMemory;
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+        if (!user) {
+            return null;
+        }
+
+        const userInMemory = User.create({
+            name: user.name,
+            email: user.email,
+            createdAt: user.createdat,
+            updatedAt: user.updatedAt,
+            password: user.password
+        }, user.id);
+        return userInMemory;
+    }
+
+}
---
+++ src/domain/entities/users/User.ts
@@ -0,0 +1,26 @@
+import { Entity } from '../../../core/domain/Entity';
+
+interface IUserProps {
+    name: string,
+    email: string,
+    password: string,
+    createdAt?: Date,
+    updatedAt?: Date,
+}
+
+export class User extends Entity<IUserProps>{
+
+    private constructor(props: IUserProps, id?: string) {
+        super(props, id);
+    }
+
+    static create(props: IUserProps, id?: string): User {
+        const user = new User({
+            ...props,
+            createdAt: props.createdAt ?? new Date(),
+        }, id);
+        return user;
+    }
+
+
+}
